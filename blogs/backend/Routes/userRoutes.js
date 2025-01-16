9b044e992ce5062ee26fdbc73ebdafb802a8fdd5
ANURAG P <anuragp212@gmail.com>
2025-01-16 10:39:47 +0530

    Update userRoutes.js

--- blogs/backend/Routes/userRoutes.js
+++ blogs/backend/Routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, register } from "../controllers/userCtrl.js";
+import { login, register } from "../controllers/userCtrl.js"; 
 //routes
 
 const router = express.Router();
